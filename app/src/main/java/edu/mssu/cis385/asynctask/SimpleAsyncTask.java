8924d1a2fc8b984630563cfddd709eea2e855dfe
Cole Cunningham <cole.dallascowboy14@gmail.com>
2021-11-30 20:27:02 -0600

    7.1 Completed

---
+++ app/src/main/java/edu/mssu/cis385/asynctask/SimpleAsyncTask.java
@@ -0,0 +1,57 @@
+package edu.mssu.cis385.asynctask;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
+    private WeakReference<TextView> mTextView;
+    private ProgressDialog sleepProgressDialog;
+
+
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running
+        int s = n * 200;
+
+        // Sleep for the random amount of time
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    @Override
+    protected void onPreExecute(){
+        sleepProgressDialog.setProgressStyle(0);
+        sleepProgressDialog.setMax(100);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+        sleepProgressDialog.setProgress(Integer.parseInt(String.valueOf(values)));
+    }
+
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+}
